5488161f2b2c832096ce847e99f8d76df5cba006
fred carter <fcarter@vantiq.com>
2018-07-26 17:09:10 -0700

    Add gradle task to run example server.
    When done via gradle task, runs for a day.  When run as a test, runs
    for 30 minutes.

---
+++ opcuaSource/src/test/java/io/vantiq/extsrc/opcua/io/vantiq/extsrc/opcua/sampleServer/RunExampleServer.java
@@ -0,0 +1,58 @@
+package io.vantiq.extsrc.opcua.io.vantiq.extsrc.opcua.sampleServer;
+
+import io.vantiq.extsrc.opcua.categories.OnlyHandTests;
+import io.vantiq.extsrc.opcua.uaOperations.OpcUaTestBase;
+import io.vantiq.extsrc.opcua.uaOperations.Utils;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.milo.examples.server.ExampleServer;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * This class is just present as a simple way run the (milo) example server as an external server.
+ * Simply running this "test" will start the server then then stall for the given amount of time.
+ *
+ */
+@Slf4j
+public class RunExampleServer {
+    public static final long DURATION_30_MINUTES = TimeUnit.MINUTES.toMillis(30);
+    public static final long DURATION_10_MINUTES = TimeUnit.MINUTES.toMillis(10);
+    public static final long DURATION_1_DAY = TimeUnit.DAYS.toMillis(1);
+
+    public static long durationOfRun = DURATION_30_MINUTES;
+
+    public void testRunAWhile() {
+        try {
+            long hours = TimeUnit.MILLISECONDS.toHours(durationOfRun);
+            long minutes = TimeUnit.MILLISECONDS.toMinutes(durationOfRun)
+                    - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(durationOfRun));
+            long seconds = TimeUnit.MILLISECONDS.toSeconds(durationOfRun)
+                    - TimeUnit.MINUTES.toSeconds(minutes) - TimeUnit.HOURS.toSeconds(hours);
+            String durationString = String.format("%d hours %d min, %d sec",
+                    hours, minutes, seconds);
+
+            ExampleServer exampleServer = new ExampleServer();
+            exampleServer.startup().get();
+
+            log.info("Running example server for {}", durationString);
+
+            Thread.sleep(durationOfRun);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted...)");
+        }
+        catch (Exception e)
+        {
+            log.error("Trapped exception during ExampleServer startup: " + Utils.errFromExc(e));
+        }
+    }
+
+    public static void main(String[] argv) {
+        durationOfRun = DURATION_1_DAY;
+        RunExampleServer server = new RunExampleServer();
+        server.testRunAWhile();
+    }
+}
