e56d3542255b3f45829dec92cca2c751098c438b
AlexBlumer <alpha.blumer@gmail.com>
2018-08-13 11:48:03 -0700

    Config handler javadoc'ed

--- objectRecognitionSource/src/main/java/io/vantiq/extsrc/objectRecognition/ObjectRecognitionConfigHandler.java
+++ objectRecognitionSource/src/main/java/io/vantiq/extsrc/objectRecognition/ObjectRecognitionConfigHandler.java
@@ -11,13 +11,45 @@ import org.slf4j.LoggerFactory;
 
 import io.vantiq.extjsdk.ExtensionServiceMessage;
 import io.vantiq.extjsdk.Handler;
-import io.vantiq.extsrc.objectRecognition.imageRetriever.CameraRetriever;
 import io.vantiq.extsrc.objectRecognition.imageRetriever.FileRetriever;
 import io.vantiq.extsrc.objectRecognition.imageRetriever.ImageRetrieverInterface;
 import io.vantiq.extsrc.objectRecognition.neuralNet.DarkflowProcessor;
 import io.vantiq.extsrc.objectRecognition.neuralNet.NeuralNetInterface;
 import io.vantiq.extsrc.objectRecognition.neuralNet.YoloProcessor;
 
+/**
+ * Sets up the source using the configuration document, which looks as below.
+ *<pre> {
+ *      extSrcConfig: {
+ *          general: {
+ *              &lt:general options&lt;
+ *          },
+ *          dataSource: {
+ *              &lt:image retriever options&lt;
+ *          },
+ *          neuralNet: {
+ *              &lt:neural net options&lt;
+ *          }
+ *      }
+ * }</pre>
+ * 
+ * The options for general are as follows:
+ * <ul>
+ *      <li>{@code pollRate}: Required. This indicates how often an image should be captured. A positive number
+ *                      represents the number of milliseconds between captures. If the specified time is less than
+ *                      the amount of time it takes to process the image then images will be taken as soon as the
+ *                      previous finishes. If this is set to 0, the next image will be captured as soon as the previous
+ *                      is sent. If this is set to a negative number, then images will be captured and processed only
+ *                      when a Query message is received.
+ * </ul>
+ * 
+ * Most options for dataSource and neuralNet are dependent on the implementation of {@link ImageRetrieverInterface} and
+ * {@link NeuralNetInterface} specified through the {@code type} option. {@code type} is the fully qualified class name
+ * of the implementation. It can also be unset, in which case it will attempt to find {@code DefaultRetriever} and 
+ * {@code DefaultProcessor}, which will be written by you, for your specific needs. {@code type} can also be set to the
+ * implementations included in the standard package, {@link FileRetriever file} and {@link CameraRetriever camera} for
+ * dataSource and {@link YoloProcessor yolo} and {@link DarkflowProcessor darkflow} for neuralNet.
+ */
 public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMessage>{
     
     Logger                  log;
@@ -35,7 +67,20 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
         log = LoggerFactory.getLogger(this.getClass().getCanonicalName() + "#" + sourceName);
     }
     
+    /*
+     * The fully qualified class names of the standard image retrievers. They are placed in strings instead of using
+     * getCanonicalName() so that the classes can be removed without requiring code edits
+     */
+    final String FILE_RETRIEVER_FQCN        = "io.vantiq.extsrc.objectRecognition.imageRetriever.FileRetriever";
+    final String CAMERA_RETRIEVER_FQCN      = "io.vantiq.extsrc.objectRecognition.imageRetriever.CameraRetriever";
     final String DEFAULT_IMAGE_RETRIEVER    = "io.vantiq.extsrc.objectRecognition.imageRetriever.DefaultRetriever";
+    
+    /*
+     * The fully qualified class names of the standard neural nets. They are placed in strings instead of using
+     * getCanonicalName() so that the classes can be removed without requiring code edits
+     */
+    final String YOLO_PROCESSOR_FQCN        = "io.vantiq.extsrc.objectRecognition.neuralNet.YoloProcessor";
+    final String DARKFLOW_PROCESSOR_FQCN    = "io.vantiq.extsrc.objectRecognition.neuralNet.DarkflowProcessor";
     final String DEFAULT_NEURAL_NET         = "io.vantiq.extsrc.objectRecognition.neuralNet.DefaultProcessor";
     
     /**
@@ -44,6 +89,7 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
     @Override
     public void handleMessage(ExtensionServiceMessage message) {
         Map<String,Object> config = (Map) message.getObject();
+        Map<String,Object> general;
         Map<String,Object> dataSource;
         Map<String,Object> neuralNetConfig;
         
@@ -54,14 +100,20 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
             return;
         }
         config = (Map) ((Map) config.get("config")).get("extSrcConfig");
+        if ( !(config.get("general") instanceof Map)) {
+            log.error("No general options specified. Exiting...");
+            failConfig();
+            return;
+        }
+        general = (Map) config.get("general");
         if ( !(config.get("dataSource") instanceof Map)) {
-            log.error("No data source specified for source ' " + sourceName + "'. Exiting...");
+            log.error("No data source specified. Exiting...");
             failConfig();
             return;
         }
         dataSource = (Map) config.get("dataSource");
         if ( !(config.get("neuralNet") instanceof Map)) {
-            log.error("No neural net specified for source ' " + sourceName + "'. Exiting...");
+            log.error("No neural net specified. Exiting...");
             failConfig();
             return;
         }
@@ -72,9 +124,9 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
         if (neuralNetConfig.get("type") instanceof String) {
             neuralNetType = (String) neuralNetConfig.get("type");
             if (neuralNetType.equals("yolo")) {
-                neuralNetType = YoloProcessor.class.getCanonicalName();
+                neuralNetType = YOLO_PROCESSOR_FQCN;
             } else if (neuralNetType.equals("darkflow")) {
-                neuralNetType = DarkflowProcessor.class.getCanonicalName();
+                neuralNetType = DARKFLOW_PROCESSOR_FQCN;
             } else if (neuralNetType.equals("default")) {
                 neuralNetType = DEFAULT_NEURAL_NET;
             }
@@ -104,9 +156,9 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
             retrieverType = (String) dataSource.get("type");
             // Translate simple types into FQCN
             if (retrieverType.equals("file")) {
-                retrieverType = FileRetriever.class.getCanonicalName();
+                retrieverType = FILE_RETRIEVER_FQCN;
             } else if (retrieverType.equals("camera")) {
-                retrieverType = CameraRetriever.class.getCanonicalName();
+                retrieverType = CAMERA_RETRIEVER_FQCN;
             } else if (retrieverType.equals("default")) {
                 retrieverType = DEFAULT_IMAGE_RETRIEVER;
             }
@@ -128,7 +180,7 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
             return;
         }
         
-        if (dataSource.get("pollRate") instanceof Integer) {
+        if (general.get("pollRate") instanceof Integer) {
             int polling = (int) dataSource.get("pollRate");
             if (polling > 0) {
                 int pollRate = polling;
@@ -161,7 +213,7 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
     }
     
     /**
-     * 
+     * Stops the host {@link ObjectRecognitionCore} and marks the configuration as completed.
      */
     private void failConfig() {
         source.stop();
@@ -177,6 +229,12 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
         return configComplete;
     }
     
+    /**
+     * Obtains the neural net specified by the given class. If the class cannot be found or properly created, then an
+     * error is logged and the configuration exits.
+     * @param className The fully-qualified class name of the implementation to be obtained.
+     * @return          An instance of the specified implementation, or null if the class could not be found.
+     */
     NeuralNetInterface getNeuralNet(String className) {
         Class<?> clazz = null;
         Constructor<?> constructor = null;
@@ -216,6 +274,12 @@ public class ObjectRecognitionConfigHandler extends Handler<ExtensionServiceMess
         return (NeuralNetInterface) object;
     }
     
+    /**
+     * Obtains the image retriever specified by the given class. If the class cannot be found or properly created, then
+     * an error is logged and the configuration exits.
+     * @param className The fully-qualified class name of the implementation to be obtained.
+     * @return          An instance of the specified implementation, or null if the class could not be found.
+     */
     ImageRetrieverInterface getImageRetriever(String className) {
         Class<?> clazz = null;
         Constructor<?> constructor = null;
