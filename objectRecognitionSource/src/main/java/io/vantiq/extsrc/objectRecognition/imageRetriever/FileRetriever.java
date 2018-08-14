e64c331a9dee1f674dbbb256c3b95efe6cd1fb47
Namir Fawaz <namirfawaz@Namirs-MacBook-Pro.local>
2018-08-14 11:58:29 -0700

    Changed File Retriever to handle .mov files. Still needs to handle other types of files

--- objectRecognitionSource/src/main/java/io/vantiq/extsrc/objectRecognition/imageRetriever/FileRetriever.java
+++ objectRecognitionSource/src/main/java/io/vantiq/extsrc/objectRecognition/imageRetriever/FileRetriever.java
@@ -4,21 +4,37 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import io.vantiq.extsrc.objectRecognition.ObjectRecognitionCore;
 import io.vantiq.extsrc.objectRecognition.exception.ImageAcquisitionException;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
 public class FileRetriever implements ImageRetrieverInterface {
 
     File defaultImageFile;
+    VideoCapture capture;
+    Boolean isMov = false;
     
     @Override
     public void setupDataRetrieval(Map<String, ?> dataSourceConfig, ObjectRecognitionCore source) throws Exception {
         if (dataSourceConfig.get("fileLocation") instanceof String) {
             String imageLocation = (String) dataSourceConfig.get("fileLocation");
-            defaultImageFile = new File(imageLocation);
-            if ( !(defaultImageFile.exists() && !defaultImageFile.isDirectory() && defaultImageFile.canRead())) {
-                throw new IllegalArgumentException ("Could not read file at '" + defaultImageFile.getAbsolutePath() + "'");
+            String fileExtension = (String) dataSourceConfig.get("fileExtension");
+            if (fileExtension.equals("mov")) {
+                isMov = true;
+                nu.pattern.OpenCV.loadShared();
+                capture = new VideoCapture(imageLocation);
+            }
+            else {
+                defaultImageFile = new File(imageLocation);
+                if ( !(defaultImageFile.exists() && !defaultImageFile.isDirectory() && defaultImageFile.canRead())) {
+                    throw new IllegalArgumentException ("Could not read file at '" + defaultImageFile.getAbsolutePath() + "'");
+                }
             }
         } else if (dataSourceConfig.get("pollRate") instanceof Integer && 
                         (Integer) dataSourceConfig.get("pollRate") >= 0) { // Won't be using messages to get the file location
@@ -28,10 +44,25 @@ public class FileRetriever implements ImageRetrieverInterface {
 
     @Override
     public byte[] getImage() throws ImageAcquisitionException {
-        try {
-            return Files.readAllBytes(defaultImageFile.toPath());
-        } catch (IOException e) {
-            throw new ImageAcquisitionException("Could not read the given file");
+        if (isMov) {
+            Mat matrix = new Mat();
+    
+            capture.read(matrix);
+          
+            MatOfByte matOfByte = new MatOfByte();
+            Imgcodecs.imencode(".jpg", matrix, matOfByte);
+            byte [] imageByte = matOfByte.toArray();
+            matOfByte.release();
+                    
+            return imageByte;
+        }
+        
+        else {
+            try {
+                return Files.readAllBytes(defaultImageFile.toPath());
+            } catch (IOException e) {
+                throw new ImageAcquisitionException("Could not read the given file");
+            }
         }
     }
 
