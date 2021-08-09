51ac5abc68d9d6bdc9423046171a1f80f2783bf0
fred carter <fcarter@vantiq.com>
2021-08-09 16:40:04 -0700

    Add tool to simulate unstable network on Mac OS

---
+++ extjsdk/src/test/resources/simulateUnstableNetwork.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Shell script for Mac OS that will turn off then on wifi to monkey with network.
+# This allows for manual testing for connector disconnection recovery.
+
+for ((i = 0; i < 100; i++)) do
+    echo Attempt $i;
+    echo disabling network;
+    networksetup -setnetworkserviceenabled Wi-Fi off;
+    echo sleeping for 10 seconds;
+    sleep 10;
+    echo reconnecting network;
+    networksetup -setnetworkserviceenabled Wi-Fi on;
+    echo sleeping for a minute;
+    sleep 60;
+done
