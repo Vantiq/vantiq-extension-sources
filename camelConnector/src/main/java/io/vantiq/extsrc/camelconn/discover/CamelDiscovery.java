44fd638853e37206226fcc59ab17792852f89d36
fred carter <fcarter@vantiq.com>
2023-02-02 16:20:06 -0800

    Remove extraneous import

--- camelConnector/src/main/java/io/vantiq/extsrc/camelconn/discover/CamelDiscovery.java
+++ camelConnector/src/main/java/io/vantiq/extsrc/camelconn/discover/CamelDiscovery.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ExtendedCamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.spi.ConfigurerResolver;
 
 import java.io.IOException;
 import java.io.InputStream;
