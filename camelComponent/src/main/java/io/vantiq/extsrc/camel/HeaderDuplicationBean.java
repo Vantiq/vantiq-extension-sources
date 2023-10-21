a0fd4206e37b6cf02684402b10d8c878c8c3b009
fred carter <fcarter@vantiq.com>
2023-10-20 18:27:55 -0700

    Remove unused logging method for bean.
    Not needed since callers can get map if required.

--- camelComponent/src/main/java/io/vantiq/extsrc/camel/HeaderDuplicationBean.java
+++ camelComponent/src/main/java/io/vantiq/extsrc/camel/HeaderDuplicationBean.java
@@ -8,7 +8,6 @@
 
 package io.vantiq.extsrc.camel;
 
-import org.slf4j.Logger;
 import java.util.Map;
 
 public class HeaderDuplicationBean {
@@ -21,16 +20,5 @@ public class HeaderDuplicationBean {
     
     public void setHeaderDuplicationMap(Map<String, String> heMap) {
         this.headerDuplicationMap = heMap;
-    }
-    
-    public void logHeaderEquivalences(Logger log) {
-        if (headerDuplicationMap == null) {
-            log.info("No header duplications found");
-        } else {
-            log.info("{} header duplication found", headerDuplicationMap.size());
-            headerDuplicationMap.forEach((k, v) -> {
-                log.info("\theader: {} mapped to {}", k, v);
-            });
-        }
     }
 }
