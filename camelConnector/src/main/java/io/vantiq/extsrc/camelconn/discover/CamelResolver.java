5301c34ca562fef895be43eb56dc0d2d5772b5f0
fred carter <fcarter@vantiq.com>
2024-03-18 16:45:05 -0700

    Add mediator & type overrides so that we can compensate for maven issues.
    Some of the things used by the atlassian/jira stuff seems to be registered
    incorrectly, so we need to be able to work around those things.  We supply
    the generic ability to provide overrides for the type for particular
    library dependendies, then override the artifact(s) required as appropriate.

--- camelConnector/src/main/java/io/vantiq/extsrc/camelconn/discover/CamelResolver.java
+++ camelConnector/src/main/java/io/vantiq/extsrc/camelconn/discover/CamelResolver.java
@@ -18,6 +18,8 @@ import org.apache.ivy.core.module.descriptor.DefaultDependencyArtifactDescriptor
 import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
 import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
 import org.apache.ivy.core.module.descriptor.DependencyArtifactDescriptor;
+import org.apache.ivy.core.module.descriptor.DependencyDescriptorMediator;
+import org.apache.ivy.core.module.id.ModuleId;
 import org.apache.ivy.core.module.id.ModuleRevisionId;
 import org.apache.ivy.core.report.ArtifactDownloadReport;
 import org.apache.ivy.core.report.ResolveReport;
@@ -25,6 +27,8 @@ import org.apache.ivy.core.resolve.ResolveOptions;
 import org.apache.ivy.core.retrieve.RetrieveOptions;
 import org.apache.ivy.core.retrieve.RetrieveReport;
 import org.apache.ivy.core.settings.IvySettings;
+import org.apache.ivy.plugins.matcher.ExactPatternMatcher;
+import org.apache.ivy.plugins.namespace.NamespaceTransformer;
 import org.apache.ivy.plugins.resolver.ChainResolver;
 import org.apache.ivy.plugins.resolver.IBiblioResolver;
 import org.apache.ivy.util.DefaultMessageLogger;
@@ -38,7 +42,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 public class CamelResolver {
@@ -55,6 +58,7 @@ public class CamelResolver {
     
     private static int unnamedResolverCount = 0;
     private static final String DEFAULT_CONFIGURATION = "default";
+    private static final String ANY_CONFIGURATION = "*";
     
     private static final Map<String, String> typeOverrides = Map.of(
             "com.atlassian.sal:sal-api:4.4.2", "jar"
@@ -93,7 +97,7 @@ public class CamelResolver {
             // Disable voluminous Ivy output that's not really helpful.
             Message.setDefaultLogger(new DefaultMessageLogger(Message.MSG_WARN));
         } else {
-            // Disable voluminous Ivy output that's not really helpful.
+            // Turn on voluminous Ivy output in trace case....
             Message.setDefaultLogger(new DefaultMessageLogger(Message.MSG_VERBOSE));
         }
         //creates clear ivy settings
@@ -112,7 +116,7 @@ public class CamelResolver {
                     aResolver.setRoot(repoUrl);
                     aResolver.setName(name + "::" + repoUrl);
                 } catch (MalformedURLException mue) {
-                    throw new IllegalArgumentException("Malformed repos URL: " + repo.toString(), mue);
+                    throw new IllegalArgumentException("Malformed repos URL: " + repo, mue);
                 }
                 
                 aResolver.setM2compatible(true);
@@ -153,8 +157,9 @@ public class CamelResolver {
         if (!log.isTraceEnabled()) {
             // Reduce the volume of output from Ivy unless we really need/want it.
             retrieveOptions.setLog(LogOptions.LOG_QUIET);
+        } else {
+            retrieveOptions.setLog(LogOptions.LOG_DEFAULT);
         }
-    
         //creates an Ivy instance with settings
         ivy = Ivy.newInstance(ivySettings);
     }
@@ -205,36 +210,94 @@ public class CamelResolver {
                                                                          false,
                                                                          false,
                                                                          true);
-        dd.addDependencyConfiguration("default", "default");
-        if (type == null) {
-            log.trace(">>>> Checking for artifact type override for {}:{}:{}", organization, name, revision);
-            String key = String.join(":", organization, name, revision);
-            String newType = typeOverrides.get(key);
-            if (newType != null) {
-                log.debug("Adding artifact type for {}:{}:{} :: {}", organization, name, revision, type);
-                type = newType;
-            }
-        }
+        dd.addDependencyConfiguration(DEFAULT_CONFIGURATION, ANY_CONFIGURATION);
+
         if (type != null) {
-            // If we have a type specified, make sure that that's the artifact we get.
             Artifact typedArtifact = new DefaultArtifact(ModuleRevisionId.newInstance(organization, name, revision),
                                                          null, name, type, "." + type);
-            log.debug("Adding type-specific artifact for {}:{}:{} :: {}", organization, name, revision, type);
+            log.debug(">>>> Specifying artifact for {}:{}", name, type);
             DependencyArtifactDescriptor dad =
                     new DefaultDependencyArtifactDescriptor(dd, name, type, type, null, null);
             dd.addDependencyArtifact(DEFAULT_CONFIGURATION, dad);
             md.addArtifact(DEFAULT_CONFIGURATION, typedArtifact);
         }
         md.addDependency(dd);
-        log.trace("Resolving required libraries -- {}", purpose);
-        //init resolve report
+        
+        // There are some dependencies that don't seem to resolve correctly, at least with our implementation.  To
+        // counter this, we track a set of places where we need to override the type provided by the maven repo.
+        // This situation typically arises when the artifact in question specifies a packaging attribute for some
+        // configurations, and the configuration for which we're locking qualifies.  To manage this, we'll add a
+        // DependencyDescriptorMediator (via the lambda below) which, when it sees the dependency specified in the
+        // typeOverrides list, will add a DependencyArtifact to force the downloading of th correct type/extension.
+        // these adjustments allow us to include having to special case lots of stuff in the kamelet/assembly
+        // specifications.  It also will provide the same functions if users configure their own camelConnectors that
+        // encounter these situations.
+        
+        typeOverrides.forEach( (src, newType) -> {
+            String[] parts = src.split(":");
+            DependencyDescriptorMediator ddm = dependencyDescriptor -> {
+                String targetOrg = dependencyDescriptor.getDependencyRevisionId().getOrganisation();
+                String targetName = dependencyDescriptor.getDependencyRevisionId().getName();
+                String targetRev = dependencyDescriptor.getDependencyRevisionId().getRevision();
+                log.trace("CamelConn mediator called with {}:{}:{}", targetOrg, targetName, targetRev);
+                // We should get called only with the correct set, but I have seen things behave differently, so
+                // better to be safe.  So here we check that the passed-in dependency (target*) match the type
+                // overrides for which this DependencyDescriptorMediator is being constructed.
+                if (parts[0].equals(targetOrg) && parts[1].equals(targetName) && parts[2].equals(targetRev)) {
+                    log.trace(">>>> Specifying artifact for {}:{}:{}:{}", targetOrg, targetName, targetRev,
+                              newType);
+                    DependencyArtifactDescriptor dad =
+                            new DefaultDependencyArtifactDescriptor(dependencyDescriptor, targetName, newType,
+                                                                    newType, null, null);
+                    if (dependencyDescriptor instanceof DefaultDependencyDescriptor) {
+                        // PomModuleDescriptorBuilder.PomDependencyDescriptor is based on
+                        // DefaultDependencyDescriptor so this covers that case as well.
+                        
+                        // In this case, just update the existing dependency
+                        ((DefaultDependencyDescriptor)
+                                dependencyDescriptor).addDependencyArtifact(ANY_CONFIGURATION, dad);
+                    } else {
+                        log.error("Unexpected DependencyDescriptor class: {}.  Attempting to reconstruct descriptor " +
+                                          "for {}:{}:{}:{}",
+                                  dependencyDescriptor.getClass().getName(), targetOrg, targetName, targetRev, newType);
+                        NamespaceTransformer identityTransformer = new NamespaceTransformer() {
+                            public ModuleRevisionId transform(ModuleRevisionId ourMrid) {
+                                return ourMrid;
+                            }
+                            public boolean isIdentity() {
+                                return true;
+                            }
+                        };
+                        DefaultDependencyDescriptor ddRecon =
+                                DefaultDependencyDescriptor.transformInstance(
+                                        dependencyDescriptor, identityTransformer, false);
+                        DependencyArtifactDescriptor dadRecon =
+                                new DefaultDependencyArtifactDescriptor(ddRecon, targetName, newType,
+                                                                        newType, null, null);
+
+                        ddRecon.addDependencyArtifact(ANY_CONFIGURATION, dadRecon);
+                        return ddRecon;
+                    }
+                    return dependencyDescriptor;
+                } else {
+                    log.trace("Ignoring mediation for {}:{}:{}", targetOrg, targetName, targetRev);
+                }
+                return dependencyDescriptor;
+            };
+            // For each typed override, add a mediator to "fix it up" as required.
+            log.debug("Adding mediator for {}:{}", parts[0], parts[1]);
+            md.addDependencyDescriptorMediator(new ModuleId(parts[0], parts[1]),
+                                               ExactPatternMatcher.INSTANCE, ddm);
+        });
+        log.info("Resolving required libraries -- {}. This may take a while.", purpose);
+
         ResolveReport report = ivy.resolve(md, resolveOptions);
         
-        if (!loadErrorsAcceptable(report)) {
+        if (report.hasError()) {
+            // If we get errors, notify our users and fail.
             throw new ResolutionException(identity() + ": Error(s) encountered during resolution: " +
                                                   String.join(", ", report.getAllProblemMessages()));
         }
-        
         if (log.isTraceEnabled()) {
             // do long-winded report only when requested
             ArtifactDownloadReport[] reps = report.getAllArtifactsReports();
@@ -267,31 +330,5 @@ public class CamelResolver {
         }
         log.trace("{} -- Making {} artifacts available to {}", identity(), necessaryFiles.size(), purpose);
         return necessaryFiles;
-    }
-
-    // Verify that all the errors in the list are contained in our list of type overrides. Every element in the error
-    // report must be resolved via that override list.
-    private boolean loadErrorsAcceptable(ResolveReport report) {
-        if (report.hasError()) {
-            List<String> errs = report.getAllProblemMessages();
-            AtomicBoolean okError = new AtomicBoolean(true); // OK until proven otherwise
-            errs.forEach( (err) -> {
-                boolean foundOKError = false;
-                for (String key: typeOverrides.keySet()) {
-                    String[] parts = key.split(":");
-                    String goodIndicator = parts[0] + "#" + parts[1] + ";" + parts[2];
-                    foundOKError = err.contains(goodIndicator);
-                    if (foundOKError) {
-                        log.warn("Resolution issues ignored for {} due to expected resolution.", key);
-                        break;
-                    }
-                }
-                if (!foundOKError) {
-                    okError.set(false);
-                }
-            });
-            return okError.get();
-        }
-        return true;
     }
 }
