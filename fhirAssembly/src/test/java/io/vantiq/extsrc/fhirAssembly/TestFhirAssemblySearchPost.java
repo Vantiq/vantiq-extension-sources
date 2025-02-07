fd260c67efd6ce98f4fc9c563a39ff7f423efb9a
fred carter <fcarter@vantiq.com>
2025-02-06 18:01:38 -0800

    Add support for conditional create/update/delete (#553)
    * Redo of interaction operations to more fully encompass the standard.
    
    Expand general parameters to include headers.  Return a FHIR Response type
    that includes headers & status.
    
    * Add types in support of passing headers & returning status
    
    * Update tests to handle new structures passed in & returned
    
    * Properly push conditionals in headers thru update & create
    
    * Add tests for conditional create & update.
    
    Update is now ignored since the HAPI server doesn't seem to support it.
    
    * Add assembly config parameter for Http method for search.
    
    Add tests for said configurations & methods.
    
    * EXTPSDK: resolve CVE/Dependabot issue re: jinja2 (#556)
    
    * Update jinja2 version
    
    * Update pypi version number
    
    * PythonExecSource: Update jinja2 dependency/ (#557)
    
    * Update jinja2 version as per dependabot.
    
    Update other, related, Vantiq dependencies.
    
    * Update package version
    
    * Fix formatting
    
    * Remove leftover debug stuff

---
+++ fhirAssembly/src/test/java/io/vantiq/extsrc/fhirAssembly/TestFhirAssemblySearchPost.java
@@ -0,0 +1,30 @@
+package io.vantiq.extsrc.fhirAssembly;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import java.util.Map;
+
+/**
+ * Test operations against a FHIR server using fhirConnection assembly
+ */
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestFhirAssemblySearchPost extends TestFhirAssembly {
+    // Playing this little indirection since the methods are static (Junit Rules) but we want to override.  Copying a
+    // bunch of code just to test different situations is not attractive.  JUnit's @BeforeClass will ensure that only
+    // one instance of a named method is run, so that suffices for our needs.
+    
+    @BeforeClass
+    public static void setupEnv() {
+        performSetup(getAssemblyConfigForTest());
+    }
+    
+    public static Map<String,?> getAssemblyConfigForTest() {
+        Map<String, ?> config =  Map.of("fhirServerBaseUrl", FHIR_SERVER,
+                                        "defaultSearchHttpMethod", "POST");
+        log.debug("Returning assembly config of: {}", config);
+        return config;
+    }
+}
